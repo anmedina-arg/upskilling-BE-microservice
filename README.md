e56415b4b3d9be22a6ba6db58d8587fa8226079c
anmedina-arg <andres.medina.arg@gmail.com>
2023-07-13 08:45:25 -0300

    finishing the second module

---
+++ README.md
@@ -0,0 +1,5 @@
+Aqui voy a ir poniendo algunos apuntes sobre el proyecto
+
+Como crear una red en docker:
+
+C:\User\Compu> docker network create [nombre_de_la_red]
--- star-wars-api-microservices/database/src/database/index.js
+++ star-wars-api-microservices/database/src/database/index.js
@@ -4,15 +4,6 @@ const { MONGO_URI } = require("../config/envs"); //<-- NO FUNCA EL .ENV
 
 const conn = mongoose.createConnection(MONGO_URI);
 
-// const Character = conn.model("Character", require("./schema/charactersSchema"));
-// const Film = conn.model("Film", require("./schema/filmsSchema"));
-// const Planet = conn.model("Planet", require("./schema/planetsSchema"));
-
-// Character.find()
-//   .populate("films", ["_id", "title"])
-//   .populate("homeworld", ["name"])
-//   .then((res) => console.log(res[0]));
-
 module.exports = {
   Character: conn.model("Character", require("./schema/charactersSchema")),
   Film: conn.model("Film", require("./schema/filmsSchema")),
