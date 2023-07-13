f9f0cb03f842b991aa874f60cd1d33240064a870
anmedina-arg <andres.medina.arg@gmail.com>
2023-07-13 12:28:08 -0300

    starting with docker compose

--- star-wars-api-microservices/planets/src/data/index.js
+++ star-wars-api-microservices/planets/src/data/index.js
@@ -1,8 +1,10 @@
-const planets = require("./planets.json");
+//const planets = require("./planets.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return planets;
+    const result = await axios.get("http://database:8004/Planet");
+    return result.data;
   },
   create: async () => {
     throw Error("hay un error al crear el planeta");
